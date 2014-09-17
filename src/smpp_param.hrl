629c4eb340503db607992d2a78dc202e28a7f6e6
K2 Informatics GmbH <k2informatics@bluewin.ch>
2014-09-17 10:08:36 +0200

    fixed file encoding warning
    set to to latin-1

--- src/smpp_param.hrl
+++ src/smpp_param.hrl
@@ -1,3 +1,4 @@
+%% -*- coding: latin-1 -*-
 %%% Copyright (C) 2009 Enrique Marcote, Miguel Rodriguez
 %%% All rights reserved.
 %%%
@@ -159,7 +160,7 @@
 %% destination_addr
 %%
 %% %@doc Specifies the destination SME address.  For mobile terminated
-%% messages, this is the directory number of the recipient MS.  IP addresses ็
+%% messages, this is the directory number of the recipient MS.  IP addresses รง
 %% are specified in "aaa.bbb.ccc.ddd" notation.  C-Octet String, Var. max 21
 %% octets.
 %% %@end
